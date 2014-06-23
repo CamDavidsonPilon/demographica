49ead5cf9fbd2e9891c3f56da54c5045aff375ca
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2014-06-23 09:28:36 -0400

    adding utils and main file. Todo: tests

---
+++ demographica.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from utils import DiscreteDistribution, name_distribution, normalize_vector
+
+TOTALS = pd.read_csv('datasets/counts_by_age_sex_name.csv', index_col=[0,1,2])
+PRIOR  = pd.read_csv('datasets/age_distribution.csv', index_col=[0])['both_sexes_percent']
+SURVIVAL = pd.Series([0.99, 0.98, 0.97, 0.96, 0.96, 0.95, 0.95, 0.94, 0.93, 0.92, 0.91, 0.85, 0.80, 0.70, 0.62, 0.56, 0.51, 0.45], index=PRIOR.index)
+
+
+class NameDistribution(object):
+
+    def __init__(self):
+        self._discrete_distribution = DiscreteDistribution(PRIOR)
+
+    def update(self,name):
+        """
+        Update the model with an observed name.
+
+        Params:
+            name: string
+
+        """
+        name = self.normalize_name(name)
+        self._discrete_distribution.update( SURVIVAL*name_distribution(name, TOTALS, PRIOR))
+        return
+
+    def updates(self, names):
+        """
+        Bulk updates.
+
+        Params:
+           names: an iterable of names.
+
+        """
+        for name in names:
+            self.update(name)
+
+    def plot(self):
+
+        ax = plt.subplot(2,1,1)
+        self.distribution.plot(kind='bar', ax=ax)
+        ax.set_title('Distribution of Ages')
+
+        #difference from prior
+        ax = plt.subplot(2,1,2)
+        (self.distribution - PRIOR).plot(kind='bar', ax=ax)
+        ax.set_title('Absolute differences in age distribution')
+        return
+
+    def normalize_name(self,name):
+        capitalize = lambda name: (name[0].upper() + name[1:].lower()).strip()
+        return " ".join( map( capitalize, name.split()))
+
+    def distribution():
+        def fget(self):
+            return normalize_vector(self._discrete_distribution.posterior)
+        return locals()
+    distribution = property(**distribution())
+
+
+
+
+if __name__=="__main__":
+
+    nd = NameDistribution()
+    for i in range(1000):
+        nd.update('Brittany')
+
+    nd.plot()
