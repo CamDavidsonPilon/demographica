a02b0ddb16c77caf65220eb47094877055eab11a
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2014-09-21 20:50:06 -0400

    adding new datasets of normalized data; redoing main functions

--- demographica.py
+++ demographica.py
@@ -1,77 +1,57 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-from utils import DiscreteDistribution, name_distribution, normalize_vector
+from string import capwords
 
-TOTALS = pd.read_csv('datasets/counts_by_age_sex_name.csv', index_col=[0,1,2])
-PRIOR  = pd.read_csv('datasets/age_distribution.csv', index_col=[0])['both_sexes_percent']
-SURVIVAL = pd.Series([0.99, 0.98, 0.97, 0.96, 0.96, 0.95, 0.95, 0.94, 0.93, 0.92, 0.91, 0.85, 0.80, 0.70, 0.62, 0.56, 0.51, 0.45], index=PRIOR.index)
+NAME_DISTRIBUTIONS = pd.read_csv('datasets/NameDistributions.csv', index_col=[0,1])
+VALID_NAMES = set(NAME_DISTRIBUTIONS.index.levels[0])
+SEX_DISTRIBUTIONS = pd.read_csv('datasets/SexDistributions.csv', index_col=[0,1])
+US_AGE = pd.read_csv('datasets/US Age Dist.csv', index_col=[0])
 
 
-class NameDistribution(object):
+def age_calculator(name_frequencies):
+    prob = 0.
+    for name, freq in name_frequencies.iteritems():
+        try:
+            prob += NAME_DISTRIBUTIONS.ix[name]*freq
+        except:
+            pass
+    return prob
+
+def sex_calculator(name_frequencies):
+    prob = 0.
+    for name, freq in name_frequencies.iteritems():
+        try:
+            prob += SEX_DISTRIBUTIONS.ix[name]*freq
+        except KeyError as e:
+            pass
+    return prob
+
+def compute_name_frequencies(names):
     """
-    Work in Progress
+    Parameters:
+        names: a list or iterable of first names
     """
-    def __init__(self):
-        self._discrete_distribution = DiscreteDistribution(PRIOR)
 
-    def update(self,name):
-        """
-        Update the model with an observed name.
+    data = pd.Series(names).map(capwords)
+    data = data.ix[data.map( lambda r: r in VALID_NAMES )]
+    data_normalized = data.groupby(data.values).count()/data.shape[0]
+    return data_normalized
 
-        Params:
-            name: string
+def pplot(age_distribution, label='Inferred age distribution', plot_US_age=True):
+    ax = age_distribution['probability'].plot(rot=25, label=label)
+    ax.set_ylabel('Percent')
+    ax.set_xlabel('Age')
+    ax.set_title('Age Distribution')
 
-        """
-        name = self.normalize_name(name)
-        self._discrete_distribution.update( SURVIVAL*name_distribution(name, TOTALS, PRIOR))
-        return
+    if plot_US_age:
+        ax = US_AGE['probability'].plot(ax=ax, rot=25, label='US age distribution')
 
-    def updates(self, names):
-        """
-        Bulk updates.
-
-        Params:
-           names: an iterable of names.
-
-        """
-        for name in names:
-            self.update(name)
-
-    def plot(self):
-
-        ax = plt.subplot(2,1,1)
-        self.distribution.plot(kind='bar', ax=ax)
-        ax.set_title('Distribution of Ages')
-
-        #difference from prior
-        ax = plt.subplot(2,1,2)
-        (self.distribution - PRIOR).round(4).plot(kind='bar', ax=ax)
-        ax.set_title('Absolute differences in age distribution')
-        return
-
-    def normalize_name(self,name):
-        """
-        Normalize a name. 
-
-        """
-        capitalize = lambda name: (name[0].upper() + name[1:].lower()).strip()
-        return " ".join( map( capitalize, name.split()))
-
-    def distribution():
-        def fget(self):
-            return normalize_vector(self._discrete_distribution.posterior)
-        return locals()
-    distribution = property(**distribution())
-
-
-
+    ax.legend()
+    return ax
 
 if __name__=="__main__":
-    """
-    WIP
-    """
-    nd = NameDistribution()
-    for i in range(1000):
-        nd.update('Brittany')
-
-    nd.plot()
+    names = open('sample_first_names.tsv').readlines()
+    name_distribution = compute_name_frequencies(names)
+    age_distribution = age_calculator(name_distribution)
+    sex_distribution = sex_calculator(name_distribution)
+    pplot(age_distribution)
