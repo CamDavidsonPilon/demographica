2eac5580aedbc590fc118e328a71f5727da88431
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2014-11-01 15:30:12 -0400

    removing requirement of matplotlib, unless you want to use plotting

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ DESC = """A Python Package to infer ages using US Census Data"""
 
 setup(
     name="demographica",
-    version='0.1.4',
+    version='0.1.5',
     description=DESC,
     long_description=DESC,
     author="Cameron Davidson-Pilon",
@@ -16,7 +16,7 @@ setup(
     license="MIT",
     packages=["demographica"],
     url="https://github.com/CamDavidsonPilon/demographica",
-    install_requires=["numpy", "pandas>=0.14", "matplotlib"],
+    install_requires=["numpy", "pandas>=0.14"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
